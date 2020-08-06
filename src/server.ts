44df5edd570a0816a05523fbd2b8395eb2236929
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-08-06 02:32:12 -0300

    Adding dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import routes from "./routes";
+import { config } from "dotenv";
 
-const PORT = 3333;
+config();
+
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Listem port ${PORT}`);
