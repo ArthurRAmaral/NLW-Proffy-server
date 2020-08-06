690010c8b7a9a2e171cc1ccbb914c9f47d018e40
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-08-05 21:24:00 -0300

    Config app to recive json
    Complementing some dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello Word");
+import express from "express";
+
+const PORT = 3333;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Listem port ${PORT}`);
+});
